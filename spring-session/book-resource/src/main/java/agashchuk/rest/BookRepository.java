1e5df14b90a232778470b994d9c81834e1a45b05
alexggg99 <agashchuk@corp.local>
2018-02-17 14:24:24 +0300

    delete book store(backend)

--- spring-session/book-resource/src/main/java/agashchuk/rest/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package agashchuk.rest;
-
-import agashchuk.model.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
